936ac7ca7e071afc7cadba1db09d7803020bcedd
Christian F. Brogaard <christian.filtenborg.brogaard@gmail.com>
2020-10-08 14:59:23 +0200

    Bounce

--- bounce/sketch.js
+++ bounce/sketch.js
@@ -1,5 +1,5 @@
 let cx = 0;
-let hastighed = 2;
+let hastighed = "myRange";
 let diameter = 50;
 let cz = 0;
 function setup() {
@@ -8,21 +8,25 @@ function setup() {
 function draw() {
     background(220);
     circle(cx, 300, diameter);
-    circle(300, cz, diameter)
+    // circle(300, cz, diameter)
 
+
+    
     if (cx > width - diameter / 2) {
-        hastighed = -2
+        position = -hastighed
     }
     if (cx < 0 + diameter / 2) {
-        hastighed = 2
+        position = hastighed
     }
-    cx = cx + hastighed
-    if (cz > height + diameter / 2) {
-        hastighed = -2
-    }
-    if (cz < 0 - diameter / 2) {
-        hastighed = 2
-    }
-    cz = cz + hastighed
+    cx = cx + position
+  
+   
+    // if (cz > height + diameter / 2) {
+    //     hastighed = -2
+    // }
+    // if (cz < 0 - diameter / 2) {
+    //     hastighed = 2
+    // }
+    // cz = cz + hastighed
 
 }
