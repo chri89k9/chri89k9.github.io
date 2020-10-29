d04cbe75ac3da0a52e33707f728e7597b0842487
Christian F. Brogaard <christian.filtenborg.brogaard@gmail.com>
2020-10-29 15:09:30 +0100

    logicalOperation

---
+++ logicalOperators/sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+
+
+function draw() {
+  background(220);
+  line(width/2, 0, width/2, height);
+  line(0, height/2, height, height/2);
+  line(0, 0, width, 0);
+  line(0, 0, 0, height);
+  line(0, height, width, height);
+  line(width, 0, width, height);
+  circle(mouseX, mouseY, 50);
+  //Feltet øverst til venstre
+  if(mouseX<=width/2||mouseY<=height/2){
+  fill(255, 255, 255)
+  }
+  //Feltet øverst til højre
+  if(mouseX>=width/2&&mouseY<=height/2){
+  fill(255,0,0)
+  }
+  //Feltet nederst til venstre
+  if(!!mouseX<=width/2&&mouseY>=height/2){
+    fill(255,0,0)
+  }
+  //Feltet nederst til højre
+  if(!mouseX>=width/2&&mouseY>=height/2){
+    (0,0,0)
+  }
+}
+
+
---
+++ loops4/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(400, 400);
+
+for(let x = 0; x <= 10; x++){
+    let y = 3 * x * x + 6 * x + 9
+    consoler.log(y)
+}
+
+}
