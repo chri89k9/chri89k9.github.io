1c55dc97e3e96f1ad9834c5c2ec338d602000e57
Christian F. Brogaard <christian.filtenborg.brogaard@gmail.com>
2020-10-29 14:02:02 +0100

    loops3

--- loops3/sketch.js
+++ loops3/sketch.js
@@ -1,22 +1,34 @@
 function setup(){
 
-let x = 2
-let y = 1
-let z = 0
+// let x = 2
+// let y = 1
+// let z = 0
 
-do{
+// do{
 
-    if(y <= 10){
-        z = x*y
-        y++
-        alert(z)
+//     if(y <= 10){
+//         z = x*y
+//         y++
+//         alert(z)
+//     }
+
+// else{
+//     x++
+//     y = 1
+// }
+
+// }while (x <= 10)
+
+//løkker i løkker
+for (let i = 2; i <= 10; i++) {
+    console.log('tabel: ${i}')
+    for (let j = 1; j <= 10; j++) {
+        let r = i * j
+        console.log(r)
     }
-
-else{
-    x++
-    y = 1
+}
 }
 
-}while (x <= 10)
-
+function draw(){
+    background(220);
 }
